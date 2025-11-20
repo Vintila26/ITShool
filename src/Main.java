82021953e0491cbffce42b37061faf49db725f79
Vintila Cristian <vintilacristian2606.com>
2025-11-20 20:38:09 +0200

    First Session

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.println("Hello to Java!");
+     //variabile
+        int age=28;
+        double waight =70.5d;
+        short age2 = 29;
+        long number = 2222022020202l;
+        float waightfloat = 70.5f;
+        boolean alive = true ;
+        char litere = 'A';
+        byte something = 0;
+
+        System.out.println("Varsta mea :" + age);
+        System.out.println("Varsta a doua :" + age2);
+        System.out.println("un numar : " + number);
+        System.out.println("Cantaresc :"+ waight);
+        System.out.println("cantaresc mai exact : "+ waightfloat);
+        System.out.println("Sunt in viata :" + alive);
+        System.out.println("Prima litera : " + litere);
+        System.out.println("printez byte:" +something);
+
+        String name = "Cristian";
+        System.out.println("Ma numesc :" + name);
+
+        int numar1 = 10 ;
+        int numar2 = 5 ;
+        int sum = 0 ;
+        sum = numar1 + numar2 ;
+        System.out.println(sum);
+        int dif= numar1 - numar2 ;
+        System.out.println(dif);
+        int mul = numar1 * numar2;
+        System.out.println(mul);
+        int sub = numar1 / numar2;
+        System.out.println(sub);
 
 
     }
